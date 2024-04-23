b2bf4438028002d3def19cdd0518aaadc5a03bc0
Richard Luo <luo.richard@gmail.com>
2024-04-23 18:26:04 -0400

    Fix tinyllava speedrun script to use full path

--- scripts/tinyllava-speedrun.sh
+++ scripts/tinyllava-speedrun.sh
@@ -9,7 +9,9 @@ pip install -e .
 pip install -e ".[train]"
 pip install flash-attn --no-build-isolation
 mkdir data
-sed -i 's|IMAGE_PATH=".*"|IMAGE_PATH="'/content/TinyLLaVABench/data'"|' scripts/tiny_llava/finetune/finetune.sh
-sed -i 's|DATA_PATH=".*"|DATA_PATH="'/content/TinyLLaVABench/data/test_finetune.json'"|' scripts/tiny_llava/finetune/finetune.sh
+current_dir=$(pwd)
+sed -i 's|IMAGE_PATH=".*"|IMAGE_PATH="'"${current_dir}/data"'"|' scripts/tiny_llava/finetune/finetune.sh
+sed -i 's|DATA_PATH=".*"|DATA_PATH="'"${current_dir}/data/test_finetune.json"'"|' scripts/tiny_llava/finetune/finetune.sh
+sed -i 's|OUTPUT_DIR=".*"|OUTPUT_DIR="'test_output'"|' scripts/tiny_llava/finetune/finetune.sh
 
 echo "Setup script done! Make sure to upload data files to created directory"
