e03c6ca7ac28af3698cca94ba6210f8f8d7afceb
Richard Luo <luo.richard@gmail.com>
2024-04-28 02:55:26 -0400

    Update setup-speedrun.sh

--- scripts/setup-speedrun.sh
+++ scripts/setup-speedrun.sh
@@ -5,6 +5,6 @@ conda activate shotluck
 pip install --upgrade pip  # enable PEP 660 support
 pip install -e .
 pip install -e ".[train]"
-pip install flash-attn==1.0.9 --no-build-isolation  # downgrade to flash attention v1 for older gpu support
+pip install flash-attn==2.5.8 --no-build-isolation  # required for H100 support
 
 echo "Setup script done! Make sure to upload data files to created directory"
